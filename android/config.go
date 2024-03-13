2ab1c9cf373278ffb8aa5e18e61fb17a18ada52a
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-13 10:37:18 +0800

    Revert "build: soong: use jemalloc by default and allow opt-in to scudo (2/4)"
    This reverts commit b66ad81fbe05fe3cac17d7102fad4a954aafb2a9.

--- android/config.go
+++ android/config.go
@@ -1091,7 +1091,7 @@ func (c *config) EnableCFI() bool {
 }
 
 func (c *config) DisableScudo() bool {
-	return !Bool(c.productVariables.Malloc_use_scudo)
+	return Bool(c.productVariables.DisableScudo)
 }
 
 func (c *config) Android64() bool {
